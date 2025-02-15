a66fe6d5f0a9a1f7c57b24aa6001cafb3e81dbc3
dinesh rd <ananddinesh00399@gmail.com>
2025-02-15 23:33:28 +0530

    new commit

---
+++ loops.js
@@ -0,0 +1,10 @@
+for(let i=1;i<=100;i++)
+    console.log("hello")
+
+console.log("using loops for objects")
+let student={
+    name:"dinesh",
+    age:21,
+}
+for(let key in student)
+    console.log(key," ",student[key])
